bb8c189f62b461880873d53d42a354c33dbcf234
dougfraga <douglas.fraga@gmail.com>
2021-07-12 17:10:41 -0300

    Test4 database heroku.

--- bay/settings.py
+++ bay/settings.py
@@ -83,12 +83,6 @@ parse_database = partial(dj_database_url.parse, conn_max_age=600)
 DATABASES = {
    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
